a6935fce046d54d2544eb6ae0b3fdfab7b147193
adamlin19940708 <kuso12610@gmail.com>
2018-05-02 15:04:03 +0800

    feature a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
                 font-weight: 100;
                 font-family: 'Lato';
             }
-
+asdasdasdas
             .container {
                 text-align: center;
                 display: table-cell;
